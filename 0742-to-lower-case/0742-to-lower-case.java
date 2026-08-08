9c526bc4c7a54fb793de2b855d68b39fd90a74d4
Mudit Chauhan <muditc57@gmail.com>
2026-08-08 14:40:33 +0530

    0742-TO-LOWER-CASE Stats: Time: 1 ms (23.8%), Space: 42.8 MB (67.81%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.java
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -1,5 +1,15 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        StringBuilder ans = new StringBuilder();
+
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+
+            if(ch >= 'A' && ch <= 'Z'){
+                ch = (char)(ch + 32);
+            }
+            ans.append(ch);
+        }
+        return ans.toString();
     }
 }
